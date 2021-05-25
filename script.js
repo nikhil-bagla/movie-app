6e2ea718827325a364ab14370e40a7b68edc61f6
nikhil-bagla <nikhilbagla707@gmail.com>
2021-05-25 16:12:15 +0530

    added tmdb logo

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
+// const API_URLTV='https://api.themoviedb.org/3/discover/tv?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
@@ -12,11 +13,11 @@ getMovies(API_URL)
 async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
-
     showMovies(data.results)
 }
 
 function showMovies(movies) {
+
     main.innerHTML = ''
 
     movies.forEach((movie) => {
